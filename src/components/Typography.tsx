d09509c669c77330f51b679fab0831527d3aa059
ftxtaylor <taylor@ftx.com>
2022-08-07 17:23:12 -0400

    update home styles

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -18,11 +18,16 @@ export const Typography: React.FC<{
 const colors = {
   primary: "rgba(255, 255, 255, 0.65)",
   title: "#FFFFFF",
+  link: "#8BEAFF",
 };
 
- // TODO better naming
+// TODO better naming
 const variants = {
   text1: { fontSize: 13, fontWeight: 500 },
   text2: { fontSize: 16, fontWeight: 600 },
   text3: { fontSize: 12, fontWeight: 400 },
+  text4: { fontSize: 14, fontWeight: 400 },
+  text5: { fontSize: 24, fontWeight: 600 },
+  text6: { fontSize: 16, fontWeight: 500 },
+  text7: { fontSize: 13, fontWeight: 400 },
 };
