7f4b4a38c0182b99924fb9c5ec2a14e491895cef
ftxtaylor <taylor@ftx.com>
2022-08-15 21:51:10 -0400

    fix mocks

--- src/components/TokenContext.tsx
+++ src/components/TokenContext.tsx
@@ -5,7 +5,6 @@ import React, {
   useMemo,
   useState,
 } from "react";
-import { mockTokens } from "../../mockResponses";
 import { Token } from "../types";
 import { getTokens } from "../utils/api";
 
@@ -14,14 +13,14 @@ const TokenContext = createContext<Record<string, Token>>({});
 export const TokenContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [tokens, setTokens] = useState(mockTokens);
-  // useEffect(() => {
-  //   // fetch tokens
-  //   (async () => {
-  //     const resp = await getTokens();
-  //     setTokens(resp ?? {});
-  //   })();
-  // }, []);
+  const [tokens, setTokens] = useState({});
+  useEffect(() => {
+    // fetch tokens
+    (async () => {
+      const resp = await getTokens();
+      setTokens(resp ?? {});
+    })();
+  }, []);
 
   return (
     <TokenContext.Provider value={tokens}>{children}</TokenContext.Provider>
